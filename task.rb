d15e81b92c0a4d747fb034d830965159a950981a
中元寺　裕也 <chugen@chuugenjiyuuya-no-MacBook-Pro.local>
2021-01-28 21:09:47 +0900

    [Add] q13 - q16 回答を追加

--- task.rb
+++ task.rb
@@ -119,14 +119,15 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
-
+  puts user_data.merge(update_data)
 end
 
 def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
-
+  key_data = data.keys
+  p key_data
 end
 
 def q15
@@ -134,6 +135,17 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
+  if data1.has_key?(:age)
+    puts "OK"
+  else 
+    puts "NG"
+  end
+
+  if data2.has_key?(:age)
+    puts "ok"
+  else
+    puts "NG"
+  end
 
 end
 
@@ -146,7 +158,9 @@ def q16
   ]
 
   # 以下に回答を記載
-
+  users.each do |user|
+  puts "私の名前は #{user[:name]} です。 年齢は #{user[:age]}です。"
+  end
 end
 
 class UserQ17
